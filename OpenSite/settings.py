a210228db7a0c1059e30107acd0f0ee972bbd91a
Marvin Karhan <marvin.karhan@gmx.de>
2025-08-08 19:27:32 +0200

    change setup to mysql

--- OpenSite/settings.py
+++ OpenSite/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'OpenSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'openbench',
+        'USER': 'root',
+        'PASSWORD': 'CHANGE_ME',  # Replace with your MySQL root (or app) password
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
